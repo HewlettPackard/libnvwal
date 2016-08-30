c914696a2c0447e7b1b87793cb9bb99da55097d2
Hideaki Kimura <hideaki.kimura@hp.com>
2016-08-30 13:10:58 -0700

    Testcase for circular memcpy

--- test/test_nvwal_util.cpp
+++ test/test_nvwal_util.cpp
@@ -17,17 +17,68 @@
  */
 #include <gtest/gtest.h>
 
-#include <cstdlib>
-#include <iostream>
+#include <cstring>
 
 #include "nvwal_test_common.hpp"
+#include "nvwal_util.h"
 
 namespace nvwaltest {
-DEFINE_TEST_CASE_PACKAGE(NvwalUtilTest, nvwaltest);
 
-TEST(NvwalUtilTest, Abort) {
+TEST(NvwalUtilTest, CircularMemcpy) {
+  nvwal_byte_t circular_buffer[512];
+  for (int i = 0; i < sizeof(circular_buffer); ++i) {
+    circular_buffer[i] = (nvwal_byte_t) i;
+  }
+
+  nvwal_byte_t dest[128];
+  std::memset(dest, 0, sizeof(dest));
+  nvwal_circular_memcpy(
+    dest,
+    circular_buffer,
+    sizeof(circular_buffer),
+    0,
+    sizeof(dest));
+
+  for (int i = 0; i < sizeof(dest); ++i) {
+    EXPECT_EQ((nvwal_byte_t) i, dest[i]);
+  }
+
+  std::memset(dest, 0, sizeof(dest));
+  nvwal_circular_memcpy(
+    dest,
+    circular_buffer,
+    sizeof(circular_buffer),
+    42,
+    sizeof(dest));
+  for (int i = 0; i < sizeof(dest); ++i) {
+    EXPECT_EQ((nvwal_byte_t) (42 + i), dest[i]);
+  }
+}
+
+TEST(NvwalUtilTest, CircularMemcpyWrap) {
+  nvwal_byte_t circular_buffer[512];
+  for (int i = 0; i < sizeof(circular_buffer); ++i) {
+    circular_buffer[i] = (nvwal_byte_t) i;
+  }
+
+  nvwal_byte_t dest[128];
+  std::memset(dest, 0, sizeof(dest));
+  nvwal_circular_memcpy(
+    dest,
+    circular_buffer,
+    sizeof(circular_buffer),
+    400,
+    sizeof(dest));
+
+  for (int i = 0; i < sizeof(dest); ++i) {
+    if (i < (512 - 400)) {
+      EXPECT_EQ((nvwal_byte_t) (i + 400), dest[i]);
+    } else {
+      EXPECT_EQ((nvwal_byte_t) (i + 400 - 512), dest[i]);
+    }
+  }
 }
 
 }  // namespace nvwaltest
 
-TEST_MAIN_CAPTURE_SIGNALS(NvwalUtilTest, nvwaltest);
+TEST_MAIN_CAPTURE_SIGNALS(NvwalUtilTest);
