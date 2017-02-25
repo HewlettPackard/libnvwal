2dc02a0935aadfeb00afd5dc5210b6a71510a317
Haris Volos <hvolos@gmail.com>
2017-02-25 09:39:35 -0800

    new parameters for epoch microbenchmark

--- experiments/epoch_microbench.py
+++ experiments/epoch_microbench.py
@@ -2,11 +2,17 @@ import os
 import re
 import sh
 import shutil
+import sys
 
 BIN="/home/volos/workspace/build/libnvwal/experiments/epoch_microbench"
-DISK_DIR="/tmp/libnvwal/microbench/disk_root"
-#DISK_DIR="/dev/shm/libnvwal/microbench/disk_root"
+DISK_DIR="/mnt/ssd/libnvwal/microbench/disk_root"
 NVM_DIR="/mnt/nvm/pmem1/libnvwal/microbench/nv_root"
+
+def create_folders():
+  if not os.path.exists(DISK_DIR):
+    os.makedirs(DISK_DIR)
+  if not os.path.exists(NVM_DIR):
+    os.makedirs(NVM_DIR)
 
 def cleanup():
   if os.path.exists(DISK_DIR):
@@ -20,8 +26,10 @@ def cleanup():
  
 def run_lsn_workload(nwriters, nthreads, nops, segment_size, mds_page_size, writer_buffer_size, nlogrec, logrec_size):
   # run command
+  create_folders()
+  cleanup()
   run = sh.Command(BIN)
-  nv_quota = segment_size * 8
+  nv_quota = segment_size * 32
   out = run("-disk_folder", DISK_DIR, 
             "-nvm_folder", NVM_DIR, 
             "-nwriters", nwriters, 
@@ -46,10 +54,8 @@ sep = ','
 runs = 5 
 nops = 1000000
 nwriters_range = [1,2,4,8,16,24,32,40,48]
-#nwriters_range = [40,48]
 nthreads = 1
-nlogrec_range = [1,2,4,8]
-#nlogrec_range = [4,8]
+nlogrec_range = [1,2,4,8,16,32,64]
 logrec_size = 128
 segment_size_range = map(lambda x:2**x, range(20,21))
 
@@ -60,7 +66,7 @@ for r in range(runs):
     for nlogrec in nlogrec_range:
       for nwriters in nwriters_range:
         nops_per_thread = nops / nwriters/nthreads
-        writer_buffer_size = 128*1024
+        writer_buffer_size = 1024*logrec_size*max(nlogrec_range)
         duration_us = run_lsn_workload(nwriters, nthreads, nops_per_thread, segment_size, mds_page_size, writer_buffer_size, nlogrec, logrec_size)
         total_bytes = nops_per_thread * nwriters * nthreads * nlogrec * logrec_size
         throughput = total_bytes * 1000000 / duration_us 
